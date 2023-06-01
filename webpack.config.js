0179aa49d38acaeb64d88f928b6555c9f07a873f
tianheg <yidajiabei@gmail.com>
2023-06-01 12:47:36 +0800

    fix: some errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,6 @@
+const crypto = require('crypto')
+const cryptoOrigCreateHash = crypto.createHash
+crypto.createHash = algorithm =>
+  cryptoOrigCreateHash(algorithm === 'md4' ? 'sha256' : algorithm)
+
 module.exports = require('neutrino')().webpack()
